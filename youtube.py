77b3fa747d64af95fd24309d1dc9b4c3678b2884
모두를 위한 인공지능 AI <85654856+dscoool@users.noreply.github.com>
2024-10-02 11:54:40 +0900

    Create youtube.py

---
+++ youtube.py
@@ -0,0 +1,9 @@
+from IPython.display import YouTubeVideo
+
+# Change the Youtube_ID with the link to your group's video.
+# 유튜브 ID를 알아내서 아래 입력해 보세요.
+# https://www.youtube.com/watch?v=KQwGBaEYExU
+# 위 예시에서 유튜브 ID는 KQwGBaEYExU 입니다.
+
+YOUTUBE_ID = '--okUL1jPjQ'
+video=YouTubeVideo(YOUTUBE_ID)
