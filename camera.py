5e7de355ada6f2efb0f9a2487e65c717df7fce1c
모두를 위한 인공지능 AI <85654856+dscoool@users.noreply.github.com>
2024-10-14 14:00:30 +0900

    카메라 다루기 | PyInstaller로 .exe 파일 생성하여 배포하기

--- camera.py
+++ camera.py
@@ -1,6 +1,6 @@
-// 카메라 출력 - 로컬에서 실행하여야 합니다
-// .py 파일로 로컬에 저장 후 실행하세요. 
-//카메라 연동은 colab이나 ipynb에서 지원하지 않습니다.
+# 카메라 출력 - 로컬에서 실행하여야 합니다
+# 반드시 .py 파일로 '로컬'에 저장 후 실행하세요. 
+# ipynb 지원불가
   
 import cv2
 
@@ -11,6 +11,16 @@ capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 while cv2.waitKey(33) < 0:
     ret, frame = capture.read()
     cv2.imshow("VideoFrame", frame)
+    # 키입력이 있으면 탈출
+    if cv2.waitKey(33) != -1:
+        break  # Exit the loop if any key is pressed
+
 
 capture.release()
 cv2.destroyAllWindows()
+
+# exe파일로 만들어서 배포하기 [pyinstaller]
+# (PyInstaller 대소문자 구별)
+# $ python -m pip install pyinstaller
+# $ python -m PyInstaller --onefile --windowed camera.py
+# .exe 파일은 dist 폴더 안에 생성됩니다.
